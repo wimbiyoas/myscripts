39683030e0a7c6a8b8b144e3ae88db2dee9fa66e
wimbiyoas <wyoashizkia@gmail.com>
2021-04-16 13:49:34 +0700

    RMX1901: Change CHATID telegram

--- RMX1901_release.sh
+++ RMX1901_release.sh
@@ -71,7 +71,7 @@ PTTG=1
 	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
-		CHATID="-1001291890692"
+		CHATID="-1001347363864"
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
